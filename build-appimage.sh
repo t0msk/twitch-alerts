10c3a5e56b0f79a047db79140fa110c0629ac4ee
Tomáš Sabo <tomsk.slovak@gmail.com>
2025-02-20 21:55:39 +0100

    better build process

--- build-appimage.sh
+++ build-appimage.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+cargo build --release
+
+mkdir -p TwitchAlerts.AppDir/usr/share/icons/hicolor/256x256/apps
+mkdir -p TwitchAlerts.AppDir/usr/bin
+
 rm TwitchAlerts.AppDir/AppRun
+
+cp target/release/twitch-alerts-rs TwitchAlerts.AppDir/usr/bin/
+cp icon.png TwitchAlerts.AppDir/usr/share/icons/hicolor/256x256/apps/twitch-alerts.png
+
+rm Twitch_Alerts-x86_64.AppImage
 
 NO_STRIP=true linuxdeploy --appdir TwitchAlerts.AppDir --desktop-file twitch-alerts.desktop --icon-file icon.png --output appimage
 
